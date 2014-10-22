9bdb102600ad12b2f463de207ce13034a30f7076
Allan Glen <allan@allanglen.com>
2014-10-22 11:22:51 -0600

    graphhack

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express')
+var exphbs = require('express3-handlebars');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.render('index');
 })
 
 app.listen(app.get('port'), function() {
